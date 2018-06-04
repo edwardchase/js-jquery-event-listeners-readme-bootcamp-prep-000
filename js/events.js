5d0835715335c0c243626da43d13c9e70df8ec8b
Edward Mixon <edwardchase@gatech.edu>
2018-06-04 22:12:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('form').on('keydown', function() {
+  $('form').on('keydown', function(key) {
     if (key.which===71) {
       alert('You have pressed g!');
     } else {
