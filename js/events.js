10ae5e9ff87e20203dd9805e8f8d116032a07628
Edward Mixon <edwardchase@gatech.edu>
2018-06-04 22:11:41 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,26 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $('form').on('keydown', function() {
+    if (key.which===71) {
+      alert('You have pressed g!');
+    } else {
+      alert('You haven\'t pressed g!');
+    }
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
 });
